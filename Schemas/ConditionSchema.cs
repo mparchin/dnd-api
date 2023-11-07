d7ef7cf4016f28082fb8eb7698aa681cd7dde640
mparchin <mmzparchin@gmail.com>
2023-11-07 16:24:14 +0330

    conditions endpoint

--- Schemas/ConditionSchema.cs
+++ Schemas/ConditionSchema.cs
@@ -6,11 +6,13 @@ namespace api.Schemas
     {
         public string Name { get; set; } = "";
         public string Description { get; set; } = "";
+        public long Time { get; set; }
 
         public ConditionSchema(Condition condition)
         {
             Name = condition.Name;
             Description = condition.Description;
+            Time = Convert.ToInt64(((condition.UpdatedOn ?? new DateTime(2020, 1, 1)).ToUniversalTime() - new DateTime(1970, 1, 1)).TotalMilliseconds);
         }
     }
 }
