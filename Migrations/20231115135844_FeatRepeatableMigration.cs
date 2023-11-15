156967dfc6beddb6ecd8be40e3a021ed4836c9cc
mparchin <mmzparchin@gmail.com>
2023-11-15 17:31:03 +0330

    feat repeatable

---
+++ Migrations/20231115135844_FeatRepeatableMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FeatRepeatableMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Repeatable",
+                table: "Feats",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Repeatable",
+                table: "Feats");
+        }
+    }
+}
