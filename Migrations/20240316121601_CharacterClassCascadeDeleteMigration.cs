3b649df8aed05973226e94f6a868aa3e9f31f197
mparchin <mmzparchin@gmail.com>
2024-03-16 15:46:51 +0330

    character class cascade delete

---
+++ Migrations/20240316121601_CharacterClassCascadeDeleteMigration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CharacterClassCascadeDeleteMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Classes_ClassId",
+                table: "Characters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClassId",
+                table: "Characters",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Classes_ClassId",
+                table: "Characters",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Classes_ClassId",
+                table: "Characters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClassId",
+                table: "Characters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Classes_ClassId",
+                table: "Characters",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
