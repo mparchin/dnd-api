dc0a5ad2b15362fd570cdc278ab16c4560815fe4
mparchin <mmzparchin@gmail.com>
2024-09-22 11:23:06 +0330

    spells from path

--- Updater.cs
+++ Updater.cs
@@ -1,45 +1,26 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using api.Models.FiveE;
 
 namespace api
 {
-    public class Updater(ILogger<Updater> logger, string spellUrl, string lookupUrl) : IDisposable
+    public class Updater(ILogger<Updater> logger, string spellPath)
     {
-        private readonly HttpClient _httpClient = new();
-
-        public void Dispose()
-        {
-            _httpClient.Dispose();
-            GC.SuppressFinalize(this);
-        }
-
         private async Task<Spell[]> FetchSpells()
         {
-            logger.LogInformation($"Fetching spells from {spellUrl}");
-            var spells = await _httpClient.GetFromJsonAsync<Spell[]>(spellUrl);
+            logger.LogInformation($"Fetching spells from {spellPath}");
+
+            var spells = JsonSerializer.Deserialize<Spell[]>(await File.ReadAllTextAsync(spellPath));
+
             if (spells is null)
             {
                 logger.LogCritical("Zero spells found");
                 throw new Exception("Zero spells found");
             }
-            logger.LogInformation($"Fetched {spells.Length} spells from {spellUrl}");
+            logger.LogInformation($"Fetched {spells.Length} spells from {spellPath}");
 
             return spells;
         }
-
-        // private async Task<Spell[]> FetchLookups()
-        // {
-        //     logger.LogInformation($"Fetching lookup from {spellUrl}");
-        //     var spells = await _httpClient.GetFromJsonAsync<Spell[]>(spellUrl);
-        //     if (spells is null)
-        //     {
-        //         logger.LogCritical("Zero spells found");
-        //         throw new Exception("Zero spells found");
-        //     }
-        //     logger.LogInformation($"Fetched {spells.Length} spells from {spellUrl}");
-
-        //     return spells;
-        //     // logger.LogInformation($"Fetching lookup at {lookupUrl}");
-        // }
 
         public async Task RunAsync()
         {
